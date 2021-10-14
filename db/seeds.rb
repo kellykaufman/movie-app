8355766df747f44771966f0c01116ba9ea8ae790
Kelly Kaufman <kellykaufman12@gmail.com>
2021-10-13 20:17:57 -0700

    Adds Ben Cumberbatch

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+actor = Actor.create(first_name: "Nathan", last_name: "Fillion", known_for: "Firefly")
+
 actor = Actor.create(first_name: "Marilyn", last_name: "Monroe", known_for: "Some Like It Hot")
-actor = Actor.create(first_name: "James", last_name: "Dean", known_for: "East of Eden")actor = Actor.create(first_name: "Charlie", last_name: "Hunnam", known_for: "Sons of Anarchy")actor = Actor.create(first_name: "Matt", last_name: "Damon", known_for: "Bourne Identity")actor = Actor.create(first_name: "Bradley", last_name: "Cooper", known_for: "The Hangover")
+
+actor = Actor.create(first_name: "James", last_name: "Dean", known_for: "East of Eden")
+
+actor = Actor.create(first_name: "Charlie", last_name: "Hunnam", known_for: "Sons of Anarchy")
+
+actor = Actor.create(first_name: "Matt", last_name: "Damon", known_for: "Bourne Identity")
+
+actor = Actor.create(first_name: "Bradley", last_name: "Cooper", known_for: "The Hangover")
+
+actor = Actor.create(first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock")
