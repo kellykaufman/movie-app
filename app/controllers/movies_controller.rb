fb5b5d1d9add142c434fc792334e9dd852a8b398
Kelly Kaufman <kellykaufman12@gmail.com>
2021-10-20 17:57:08 -0700

    Adds create and update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,7 +10,7 @@ class MoviesController < ApplicationController
   end
 
   def create
-    movie = { title: params[:title], year: params[:year], plot: params[:plot] }
+    movie = { title: params[:title], year: params[:year], plot: params[:plot], director: params[:director], english: params[:english] }
     render json: movie.as_json
   end
 
@@ -19,6 +19,8 @@ class MoviesController < ApplicationController
     movie.title = params[:title] || movie.title
     movie.year = params[:year] || movie.year
     movie.plot = params[:plot] || movie.plot
+    movie.director = params[:director] || movie.director
+    movie.english = params[:english] || movie.english
     render json: movie.as_json
   end
 
--- db/migrate/20211021002351_add_english_to_movie.rb
+++ db/migrate/20211021002351_add_english_to_movie.rb
@@ -1,6 +1,6 @@
 class AddEnglishToMovie < ActiveRecord::Migration[6.1]
   def change
     add_column :movies, :english, :string
-    change_column :movies, :english, :boolean
+    change_column_default :movies, :english, :true
   end
 end
