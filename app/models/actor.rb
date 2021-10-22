ff64f420d07cd97e0c77b4c506a3c1fb60a08e2a
Kelly Kaufman <kellykaufman12@gmail.com>
2021-10-21 17:20:19 -0700

    Adds validations

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,9 @@
 class Actor < ApplicationRecord
+  validates :first_name, presence: true, length: { in: 2..12 }
+
+  validates :last_name, presence: true, length: { in: 2..12 }
+
+  validates :known_for, presence: true
+
+  validates :age, numericality: { only_integer: true, greater_than: 13 }
 end
