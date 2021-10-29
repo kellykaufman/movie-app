1ca39c7d2595c837c86fff49231cb58be21686c3
Kelly Kaufman <kellykaufman12@gmail.com>
2021-10-28 18:36:48 -0700

    attempts correct model associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
   has_many :actors
+  belongs_to :genre
 
   validates :title, presence: true
   validates :year, presence: true, numericality: { only_integer: true }
