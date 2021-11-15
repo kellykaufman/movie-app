59d5e969a2c4baf28d46e05a8f0fd21b02560b18
Kelly Kaufman <kellykaufman12@gmail.com>
2021-11-14 17:50:52 -0800

    Adds axios to js file

--- public/index.js
+++ public/index.js
@@ -1,1 +1,8 @@
-<script src="index.js" defer></script>;
+// Was I supposed to put the defer here? I originally did, but that doesn't feel right. I can't remember how to link my js to my html. WHen I searched it only has
+// script tag with defer
+/* global axios */
+
+axios.get("http://localhost:3000/api/actors").then(function (response) {
+  var actors = response.data;
+  console.log(actors);
+});
